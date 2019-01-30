5eb41c8899f1aadeaa230827463b0c2a8a128576
Florence <42698925+Re4LDeaL@users.noreply.github.com>
2019-01-30 13:54:42 +0100

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,24 @@
+public class Monster {
+
+    private int lives;
+    private String name;
+
+    public void setLives(int lives){
+        this.lives=lives;
+    }
+
+    public int getLives(){
+        return lives;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+
+
+
+}
